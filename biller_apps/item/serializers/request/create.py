000ec6f7bfb0a616d5c7819490c96a5d7fddb899
Anshul <k0anshul@gmail.com>
2026-08-04 11:09:37 +0530

    item hsn and tax code update

--- biller_apps/item/serializers/request/create.py
+++ biller_apps/item/serializers/request/create.py
@@ -8,12 +8,12 @@ class ItemRequestSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=100)
     description = serializers.CharField(max_length=350, required=False, default="", allow_blank=True)
     bar_qr_code = serializers.CharField(max_length=50, required=True,allow_blank=True)
-    brand_code = serializers.CharField(max_length=10,required=True)
+    brand_code = serializers.CharField(max_length=10,required=False, default=None, allow_blank=True)
     category_code = serializers.CharField(max_length=10,required=True)
     supplier_code = serializers.CharField(max_length=10,required=True)
     image_url = serializers.URLField(required=False, default=None, allow_blank=True)
-    tax_code = serializers.CharField(max_length=100, required=True)
-    hsn_code = serializers.CharField(max_length=100, required=True)
+    tax_code = serializers.CharField(max_length=100, required=False, default=None, allow_blank=True)
+    hsn_code = serializers.CharField(max_length=100, required=False, default=None, allow_blank=True)
 
     def create(self, validated_data) -> ItemRequest:
         return ItemRequest(**validated_data)
